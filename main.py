4214cf88eda05727e8edd5b57ad38dde22b68b7f
Kaustubh <22222@iiitu.ac.in>
2024-06-29 01:45:53 +0530

    logging and utils

---
+++ main.py
@@ -0,0 +1,3 @@
+from Text_Essence.logging import logger
+
+logger.info("Welcome to our custom log")
--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
-from setuptools import find_packages,setup
-from typing import List
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 
-HYPEN_E_DOT="-e ."
-def get_requirements(file_path:str)->List[str]:
-    """"
-    this function will return the list of nrequirements
-    """
-    requirements=[]
-    with open(file_path) as file_object:
-        requirements=file_object.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+__version__ = "0.0.0"
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
+REPO_NAME = "Text-Essence-Project"
+AUTHOR_USER_NAME = "Kaustubh0801"
+SRC_REPO = "Text-Essence"
+AUTHOR_EMAIL = "22222@iiitu.ac.in"
 
-setup(
 
-name="Text-Essence",
-version="0.0.0",
-author="Kaustubh",
-author_email="22222@iiitu.ac.in",
-packages=find_packages(),
-install_requires=get_requirements("requirements.txt")
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A NLP project for Text-Summarization",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
